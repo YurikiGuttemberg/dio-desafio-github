f6d1a0d04464da4fbfde5b5ebdeeffbfbd8754f8
YurikiGuttemberg <yuriki.gutemberg@gmail.com>
2022-02-07 14:19:11 -0300

    continuação do programinha de aprendizado java

--- estudos/MyFirstProgram/src/dio/com/MyFisrtProgram.java
+++ estudos/MyFirstProgram/src/dio/com/MyFisrtProgram.java
@@ -1,9 +1,14 @@
 package dio.com;
 
+import dio.com.base.Order;
+
 public class MyFisrtProgram {
 
 	public static void main(String[] args) {
-
+		
+		Order order = new Order("Code1234");
+		
+		System.out.println(order);
 	}
 
 }
---
+++ estudos/MyFirstProgram/src/dio/com/base/Order.java
@@ -0,0 +1,15 @@
+package dio.com.base;
+
+public class Order {
+	private final String code;
+	
+	public Order(String code) {
+		
+		this.code=code;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [code=" + code + "]";
+	}
+}
